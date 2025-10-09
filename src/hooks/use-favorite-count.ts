48c6f9f9bdc6d8482bacb61f0bdf0769d498921d
IlyaMuralha <muralhakuzmenko@gmail.com>
2025-10-09 21:42:59 +0500

    рефакторит useFavoriteCount

--- src/hooks/use-favorite-count.ts
+++ src/hooks/use-favorite-count.ts
@@ -3,14 +3,16 @@ import { favoritesSelectors } from '../store/slices/favorites';
 import { useAppDispatch, useAppSelector } from './store';
 import { RequestStatus } from '../const';
 import { fetchFavorites } from '../store/api-actions';
+import { useAuth } from './user-authorization';
 
 export function useFavoriteCount() {
   const dispatch = useAppDispatch();
+  const isAuthorized = useAuth();
   const count = useAppSelector(favoritesSelectors.favorites).length;
   const status = useAppSelector(favoritesSelectors.favoritesStatus);
 
   useEffect(() => {
-    if (status === RequestStatus.Idle) {
+    if (isAuthorized && status === RequestStatus.Idle) {
       dispatch(fetchFavorites());
     }
   }, [status, dispatch]);
